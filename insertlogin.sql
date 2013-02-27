67115f7b017e9141e344599b76aaa7baa1f737b5
kwanyin <kwanyin@ualberta.ca>
2013-02-27 14:59:00 -0700

    Add some sql files

---
+++ insertlogin.sql
@@ -0,0 +1,5 @@
+insert into users values('name1', '11111', 'a', '23FEB13');
+insert into users values('name2', '22222', 'p', '25FEB13');
+insert into users values('name3', '33333', 'p', '25FEB13');
+insert into users values('name4', '44444', 'p', '25FEB13');
+commit;
