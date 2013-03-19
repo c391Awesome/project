34d1964d6fd1ff2511f0c487c860210e59efe8dc
Alex Wilson <aewilson@ualberta.ca>
2013-03-19 16:54:13 -0600

    Add file for inserting data into persons table.

---
+++ sql/insertpersons.sql
@@ -0,0 +1,6 @@
+insert into persons values('name1', 'name', 'one', '123 fake street', 'one@cool.com', '1234567890');
+insert into persons values('name2', 'name', 'two', '123 fake street', 'two@cool.com', '1234567890');
+insert into persons values('name3', 'name', 'three', '123 fake street', 'three@cool.com', '1234567890');
+insert into persons values('name4', 'name', 'four', '123 fake street', 'four@cool.com', '1234567890');
+commit;
+
