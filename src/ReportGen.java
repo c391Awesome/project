ea1183d26195079bb081b84ea5837386dc2140a3
Zhexin Tang <zhexin@ualberta.ca>
2013-03-23 13:22:14 -0600

    Refine report generation module

---
+++ src/ReportGen.java
@@ -0,0 +1,66 @@
+package ca.awesome;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ReportGen {
+
+	private String diagnosisName;
+	private String fromDate;
+	private String toDate;
+	private ArrayList<String> queryResult;
+
+	public ReportGen(String diagnosis, String fromDate, String toDate) {
+		this.diagnosisName = diagnosis;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+		this.queryResult = new ArrayList<String>();
+	}
+
+	public ArrayList<String> getArrayList() {
+		return queryResult;
+	}
+
+	public boolean getQueryResult(DatabaseConnection connection) {
+		ResultSet results = null;
+		Statement statement = null;
+		try {
+			statement = connection.createStatement();
+			results = statement.executeQuery(
+				"select first_name, last_name, address, phone, test_date"
+				+ " from persons, radiology_record where persons.user_name = radiology_record.patient_name" 
+				+ " and radiology_record.diagnosis = '" + diagnosisName + "'"
+				//+ "' and radiology_record.test_date > '" + fromDate
+				//+ "' and radiology_record.test_date < '" + toDate + "'"
+			);
+			
+			if (results == null || !results.next()) {
+				//throw new RuntimeException("failed to getQueryResult()");
+				//System.out.println("No result is found!");
+				return false;
+			}
+			
+			while(results.next()){
+
+         		//Retrieve by column name
+         		queryResult.add(results.getString("first_name"));
+         		queryResult.add(results.getString("last_name"));
+         		queryResult.add(results.getString("address"));
+         		queryResult.add(results.getString("phone"));
+				queryResult.add(results.getDate("test_date").toString());
+      		}
+			connection.close();
+			return true;
+		} catch (SQLException e) {
+			connection.close();
+			throw new RuntimeException("failed to getQueryResult()", e);
+		}
+
+	}
+	
+}
+
+
--- web/reportGen.jsp
+++ web/reportGen.jsp
@@ -35,7 +35,7 @@
 	<fieldset><legend>Report Searching Information:</legend>
 	<TABLE>
 	<TR VALIGN=TOP ALIGN=LEFT><TD><B><I>Diagnosis:</I></B></TD><TD>
-	<INPUT TYPE="text" size=50 NAME="DIAGNOSIS" VALUE="Diagnosis"><BR></TD></TR>
+	<INPUT TYPE="text" size=50 NAME="DIAGNOSIS" ><BR></TD></TR>
 	<TR VALIGN=TOP ALIGN=LEFT><TD><B><I>Start Date:</I></B></TD><TD>
 	<INPUT TYPE="text" id="from" NAME="TIME_FROM" VALUE="Select a Date"><BR></TD></TR>
 	<TR VALIGN=TOP ALIGN=LEFT><TD><B><I>End Date:</I></B></TD><TD>
@@ -44,6 +44,48 @@
 	</fieldset>
 	<INPUT TYPE="submit" NAME="GenReport" VALUE="Generate Report">
 	</FORM>
+
+	<%@ page import="java.sql.*,ca.awesome.*" %>
+	<% 
+
+		ReportGen report = new ReportGen(request.getParameter("DIAGNOSIS"),
+										request.getParameter("TIME_FROM"),
+										request.getParameter("TIME_UNTIL"));
+		%>
+	<p><b>The following are the search result for <%=request.getParameter("DIAGNOSIS")%> 
+	between <%=request.getParameter("TIME_FROM")%> and <%=request.getParameter("TIME_UNTIL")%></b></p><br>
+
+	<TABLE border="1">
+		<TR VALIGN=TOP ALIGN=LEFT>
+			<TD><B><I>First Name</I></B></TD>
+			<TD><B><I>Last Name</I></B></TD>
+			<TD><B><I>Address</I></B></TD>
+			<TD><B><I>Phone Number</I></B></TD>
+			<TD><B><I>Test Date</I></B></TD>
+		</TR>
+	<%
+		DatabaseConnection dbc = new DatabaseConnection();
+		dbc.initialize(getServletContext());
+		dbc.connect();
+		if(report.getQueryResult(dbc)) {
+	%>
+		<TR VALIGN=TOP ALIGN=LEFT>
+		<%
+		for (String temp : report.getArrayList()) {
+		%>
+		
+			<TD><B><I><%=temp%></I></B></TD>
+
+		<%
+		}
+		%>
+			</TR>
+			</TABLE>
+		<%
+		} else 
+		%>
+		<p><b>Result Not Found!</b></p>	
+
 
 </BODY>
 
