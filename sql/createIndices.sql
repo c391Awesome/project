e2077f41451014b2321784f94655a7e08f1cdf66
Alex Wilson <aewilson@ualberta.ca>
2013-04-03 16:20:42 -0600

    Fix createIndices.sql

--- sql/createIndices.sql
+++ sql/createIndices.sql
@@ -1,23 +1,11 @@
 CREATE INDEX searchIndex0 ON radiology_record(patient_name)
 INDEXTYPE IS CTXSYS.CONTEXT;
+@drjobdml  'patient_name' 2
+
 CREATE INDEX searchIndex1 ON radiology_record(diagnosis)
 INDEXTYPE IS CTXSYS.CONTEXT;
+@drjobdml  'diagnosis' 2
+
 CREATE INDEX searchIndex2 ON radiology_record(description)
 INDEXTYPE IS CTXSYS.CONTEXT;
-
-
-REM based on drjobdml.sql example script
-
-
-declare
-	job number;
-begin
-	dbms_job.submit(job, 'ctx_ddl.sync_index(''searchIndex0'');',
-						interval=>'SYSDATE+240/1440');
-	dbms_job.submit(job, 'ctx_ddl.sync_index(''searchIndex1'');',
-						interval=>'SYSDATE+240/1440');
-	dbms_job.submit(job, 'ctx_ddl.sync_index(''searchIndex2'');',
-						interval=>'SYSDATE+240/1440');
-end;
-
-commit;
+@drjobdml  'description' 2
